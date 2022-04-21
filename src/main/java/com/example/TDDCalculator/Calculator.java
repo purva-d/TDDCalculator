3fede580cf3046640a899b38d7c95407dd3428da
purva-d <purvadandekar98@gmail.com>
2022-04-21 20:16:36 +0530

    supports custom deliemeter and handles negative input

--- src/main/java/com/example/TDDCalculator/Calculator.java
+++ src/main/java/com/example/TDDCalculator/Calculator.java
@@ -1,27 +1,56 @@
 package com.example.TDDCalculator;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Calculator {
-	
-	private int getSum(String[] numbers) {
-		int sum=0;
-		for (String strNum :numbers) {
-			sum += Integer.parseInt(strNum);
-		}
-		return sum;
-	}
-	
-	public int calculate(String input) {
-		String[] numbers = input.split(",");
-		if(input.isEmpty()) {
+
+	public int calculate(String input) throws Exception {
+
+		if (input.isEmpty() || input.isBlank()) {
 			return 0;
 		}
-		if(input.strip().length() == 1) {
+
+		String[] numbers = tokenize(input.strip());
+		if (numbers.length == 1) {
 			return Integer.parseInt(input.strip());
-		}
+		} 
 		else {
 			return getSum(numbers);
 		}
+
+	}
+
+	private int getSum(String[] numbers) throws Exception {
+		int sum = 0;
+		for (String strNum : numbers) {
+			if(Integer.parseInt(strNum)> 0)
+				sum += Integer.parseInt(strNum);
+			else
+				throw new Exception("Negative numbers are not allowed");
+		}
+		return sum;
+	}
+
+	private String[] tokenize(String input) {
+		String defaultDelimeter = ",|\n";
 		
+		if(input.startsWith("//")) {
+			Matcher m = Pattern.compile("//(.*)\n(.*)").matcher(input);
+			m.matches();
+			String customDelimeter = m.group(1);
+			//System.out.println(customDelimeter);
+			String numbers = m.group(2);
+			//System.out.println(numbers);
+			// Following line is written so that it supports custom as well default delimeter ","
+			// as we are already using newline deliemter in different context here
+			String finalDelimeter = customDelimeter+"|,";
+			return numbers.split(finalDelimeter);
+		}
+		else {
+		String[] numbers = input.strip().split(defaultDelimeter);
+		return numbers;
+		}
 	}
 
 }
--- src/test/java/com/example/TDDCalculator/Calculator_test.java
+++ src/test/java/com/example/TDDCalculator/Calculator_test.java
@@ -6,26 +6,48 @@ import org.junit.jupiter.api.Test;
 
 
 class Calculator_test {
-
 	
 	private final Calculator calculator = new Calculator();;
 
+//	@Test
+//	public void emptyStringReturnsZero() throws Exception{
+//		assertEquals(calculator.calculate(""),0);
+//	}
+	
+//	@Test
+//	public void singleValueReturnsItself() throws Exception{
+//		//Calculator calculator = new Calculator();
+//		assertEquals(calculator.calculate("6"),6);
+//	}
+	
+//	@Test
+//	public void twoNumberCommaSeparatedReturnsSum() throws Exception {
+//		assertEquals(calculator.calculate("5,6"),11);
+//	}
+	
+//	@Test
+//	public void fourNumbersCommaSeparatedReturnsSum() throws Exception{
+//		assertEquals(calculator.calculate("8,2,5,6"),21);
+//	}
+	
+//	@Test
+//	public void twoNumberNewLineSeparatedReturnsSum() throws Exception{
+//		assertEquals(calculator.calculate("8,12"),20);
+//	}
+	
+//	@Test
+//	public void fourNumberDelimetedReturnsSum() throws Exception{
+//		assertEquals(calculator.calculate("5\n3,8\n12"),28);
+//	}
+	
 	@Test
-	public void emptyStringReturnsZero() {
-		assertEquals(calculator.calculate(""),0);
+	public void customDelimeter() throws Exception {
+		assertEquals(calculator.calculate("//;\n1;2;3;4" ),10);
 	}
+	
 	@Test
-	public void singleValueReturnsItself() {
-		//Calculator calculator = new Calculator();
-		assertEquals(calculator.calculate("6"),6);
-	}
-	@Test
-	public void twoNumberCommaSeparatedReturnsSum() {
-		assertEquals(calculator.calculate("5,6"),11);
-	}
-	@Test
-	public void fourNumbersCommaSeparatedReturnsSum() {
-		assertEquals(calculator.calculate("8,2,5,6"),21);
+	public void negativeInputThrowsException() throws Exception {
+		calculator.calculate("-2,4" );
 	}
 
 }
